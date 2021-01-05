fb8971b4c4c8db13be6e5a2c69a684c89a4e4914
ArthurWuTW <lion811004@gmail.com>
2021-01-05 16:59:34 +0800

    show all identities

--- client-side-file/rsa_key_gen_install_script.sh
+++ client-side-file/rsa_key_gen_install_script.sh
@@ -3,6 +3,7 @@ cd ~/.ssh
 ssh-keygen -t rsa
 rm ~/Backup/authorized_keys
 cat id_rsa.pub >> ~/Backup/authorized_keys
+ssh-add -l # show all identities
 ssh-add -D #remove all ssh identities
 ssh-add #add ssh identity
 scp ~/Backup/authorized_keys user@10.1.1.9:/home/user/.ssh

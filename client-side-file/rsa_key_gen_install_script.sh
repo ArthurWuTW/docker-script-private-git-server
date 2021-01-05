576432baeb0a5a7d2050f1f752b606cc7ae15aba
ArthurWuTW <lion811004@gmail.com>
2021-01-05 16:58:55 +0800

    test and change file name

--- client-side-file/rsa_key_gen_install_script.sh
+++ client-side-file/rsa_key_gen_install_script.sh
@@ -3,5 +3,6 @@ cd ~/.ssh
 ssh-keygen -t rsa
 rm ~/Backup/authorized_keys
 cat id_rsa.pub >> ~/Backup/authorized_keys
-ssh-add
+ssh-add -D #remove all ssh identities
+ssh-add #add ssh identity
 scp ~/Backup/authorized_keys user@10.1.1.9:/home/user/.ssh

0841cbdcc028965ef7a656814605dc437e9f4e2b
ArthurWuTW <lion811004@gmail.com>
2021-01-19 18:23:08 +0800

    avoid env exposure

---
+++ server-side-file/pass_connection_num_to_web_server.sh
@@ -0,0 +1,22 @@
+
+source env.sh
+echo $SECRET_KEY_SSH
+
+while :
+do
+
+  ss | grep -i ssh > ./a.txt
+  lines=`wc -l ./a.txt`
+  echo "hi"
+  echo $lines
+
+  print_output=($lines)
+  number=${print_output[0]}
+  echo $number
+
+  curl -X POST -H "Content-Type: application/json" \
+ -d '{"raspberry_secret_key":"'$SECRET_KEY_SSH'", "number":'$number', "server_name":"Backup"}' \
+ https://plantmonitor.mooo.com/connectionsNumReceiver
+ 
+  sleep 60
+done
